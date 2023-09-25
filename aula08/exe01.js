9e11f5418de43dcafe33cc4bda8bf7dade511740
alexsandroata <144482457+alexsandroata@users.noreply.github.com>
2023-09-24 23:02:57 -0400

    update

---
+++ aula08/exe01.js
@@ -0,0 +1,8 @@
+var vel = 12
+console.log(`seu carro esta a ${vel}Km/h`)
+
+if (vel > 60) {
+  console.log('VOCE FOU MULTADO')
+}
+
+console.log('dirija com cuidado')
---
+++ aula08/exe02.js
@@ -0,0 +1,10 @@
+var pais = 'Brasil'
+
+console.log(`Seu país de origem é ${pais}`)
+
+if (pais == 'Brasil') {
+  console.log('Voce é brasileiro')
+}
+else {
+  console.log('Voce é estrangeiro')
+}
