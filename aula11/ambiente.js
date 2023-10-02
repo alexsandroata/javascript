e691ceebb932852628b5ad3c6360e6dfadc6da6f
Alexsandro dos Santos Ramos <144482457+alexsandroata@users.noreply.github.com>
2023-10-01 22:22:36 -0300

    Update ambiente.js

--- aula11/ambiente.js
+++ aula11/ambiente.js
@@ -1,1 +1,19 @@
-console.log('tudo bem')
+do {
+    console.log('tudo bem')
+    c++
+} while(c <= 6)
+
+
+// var c = 1
+// while (c <= 6) {
+//     console.log('tudo bem')
+//     c++
+// }
+
+
+// console.log('tudo bem')
+// console.log('tudo bem')
+// console.log('tudo bem')
+// console.log('tudo bem')
+// console.log('tudo bem')
+// console.log('tudo bem')
