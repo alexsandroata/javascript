52b2f83e9361eb06f1a3e618cbd2cb7e5c5b86fb
Alexsandro dos Santos Ramos <144482457+alexsandroata@users.noreply.github.com>
2023-10-05 08:45:17 -0300

    update

--- projetos/projeto-tabuada/js/script.js
+++ projetos/projeto-tabuada/js/script.js
@@ -1,6 +1,23 @@
 var bnt = document.querySelector('input#bnt')
-bnt.addEventListener('click' tabuada)
+bnt.addEventListener('click', tabuada)
 
 function tabuada () {
-    window.alert('foi')
+    let num = document.querySelector('input#txtn')
+    let tab = document.querySelector('select#seltab')
+
+    if (num.value.length == 0){
+        window.alert('Por favor, digite um numero!')
+    }
+    else{
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        
+        while(c <= 10){
+            let item = document.createElement('option')
+            item.text = `${n}X${c}=${n*c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
 }
