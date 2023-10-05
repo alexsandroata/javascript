eac084f712dd232d0f560e59d68495984a371e3f
Alexsandro dos Santos Ramos <144482457+alexsandroata@users.noreply.github.com>
2023-10-05 10:24:25 -0300

    Update ambiente.js

--- aula12/ambiente.js
+++ aula12/ambiente.js
@@ -1,3 +1,14 @@
-let num = []
-
+let num = [5, 8, 2, 9, 3]
+num.push(1)//Cria um novo vetor no final
+num.sort() //Coloca os valores do vetor em ordem crescente
 console.log(num)
+console.log(`O vetor tem o tamanho de ${num.length} espaços`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+for (pos = 0 ; pos < num.length; pos++) {
+    console.log(num[pos])
+}
+
+for (pos in num) {//Melhor maneira de usar o for em vetores/Obj
+    console.log(num[pos])
+}
